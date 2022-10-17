7c2b85744a38404d9e70f2d5bd846076e8a473c2
ShellyBrady <mchellebr@hotmail.com>
2022-10-17 17:36:53 +0000

    Begin text-based rpg story

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("""Welcome to Your Next Adventure!
+Decide on your next action and see where you end up.""")
+
+print("""One day,)
