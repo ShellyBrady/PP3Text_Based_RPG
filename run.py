434ea64de3a326fc839412a9048844d967f91f76
ShellyBrady <mchellebr@hotmail.com>
2022-10-20 16:58:47 +0000

    add if elif else statements for game choices

--- run.py
+++ run.py
@@ -3,26 +3,42 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 
-def choice1():
+def game():
     print("""Welcome to Your Next Adventure!
         Every decision you make along the 
         way will determine how your story goes.
+        """)
 
-        Its time for your monthly guilt-induced jog.
+    print("""Its time for your monthly guilt-induced jog.
         You put on your barely worn runners and head for the door.
         You pause with your hand on the door knob.
-        Type stay or type go
         """)
+
+    answer = input("Type 'stay' or type 'go':")
+    if answer.lower == 'go':
+        print("Better get going before it starts to get dark!")
+        start = True  
+    elif answer.lower == 'stay':
+        print("Maybe tomorrow. It looks like it might rain.")
+    else:
+        print("Type only 'stay' or 'go':")
+
+if start == True:
+    print("You head for the quiet county road at the end of your drive.")   
+    print("Left will bring you through a hilly narrow road surrounded only by fields")     
+    print("Right brings you through a quiet village to the busy main road")
+    choice=input("Choose to go 'left' or 'right':")
+
+if choice == 'left':  
+    choice1 = True
+    print("Less cars but more hills..your legs will hurt tomorrow!")
+elif choice == 'right':
+    print("Right you are! Keep a steady pace")  
+else:
+    print("Type only 'left' or 'right':")      
+
     
-    c1 = input()
-    data_str = input ("Enter your data here:")
-    ans = 'one'
-    while (ans == 'one'):
-        if (c1() == 'stay'):
-            print("\nTomorrow, you can do it tomorrow.")    
-            ans = "correct"
-            choice2()
-        else:
-            print("Type either stay or go") 
-            c1= input()
+
+      
+    
 
