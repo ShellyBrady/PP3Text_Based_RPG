320e45195bd65310b8f28c282027439aa2c5df25
ShellyBrady <mchellebr@hotmail.com>
2022-10-20 23:00:02 +0000

    Restart from beginning, using python tutor to check if it works

--- run.py
+++ run.py
@@ -1,44 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+print("""Welcome to Your Next Adventure!\n
+Every decision you make along the way will determine how your story goes.\n""")
+print()
+name = input("What shall I call you?\n")
 
-def game():
-    print("""Welcome to Your Next Adventure!
-        Every decision you make along the 
-        way will determine how your story goes.
-        """)
+print("\nWell hello! Nice to meet you, " + name)
+print("""\nGood news! Its time for your monthly guilt-induced jog.\n
+Put on your barely worn runners and head for the door.\n
+Are you ready to go?\n""")
 
-    print("""Its time for your monthly guilt-induced jog.
-        You put on your barely worn runners and head for the door.
-        You pause with your hand on the door knob.
-        """)
+choice1 = input("Type 'no' or type 'yes':")
 
-    answer = input("Type 'stay' or type 'go':")
-    if answer.lower == 'go':
-        print("Better get going before it starts to get dark!")
-        start = True  
-    elif answer.lower == 'stay':
-        print("Maybe tomorrow. It looks like it might rain.")
+while True:
+
+    if choice1 == 'yes':
+        print("\nBetter get going before it starts to get dark!\n")
+        break
+    if choice1 == 'no':
+        print("\nMaybe tomorrow. It looks like it might rain.\n")
+        break
     else:
-        print("Type only 'stay' or 'go':")
-        
-    if start == True:
-            print("You head for the quiet county road at the end of your drive.")   
-            print("Left will bring you through a hilly narrow road surrounded only by fields")     
-            print("Right brings you through a quiet village to the busy main road")
-            choice=input("Choose to go 'left' or 'right':")
+        choice1 = input("Type 'no' or type 'yes':")
 
-if choice == 'left':  
-    choice1 = True
-    print("Less cars but more hills..your legs will hurt tomorrow!")
-elif choice == 'right':
-    print("Right you are! Keep a steady pace")  
-else:
-    print("Type only 'left' or 'right':")      
+print("Head for the quiet county road at the end of your drive.\n")
+print("Left will bring you through a hilly narrow road surrounded by fields\n")
+print("Right brings you through a quiet village to the busy main road\n")
 
+choice2 = input("Choose to go 'left' or 'right':")
 
+while True:
 
-      
-    
+    if choice2 == 'left':
+        print("\nLess cars but more hills..your legs will hurt tomorrow!\n")
+        break
+    if choice2 == 'right':
+        print("\nRight you are! Keep a steady pace\n")
+        break
+    else:
+        choice2 = input("Choose to go 'left' or 'right':")
 
