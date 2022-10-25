73de2db934d360a623ab5dc575d2c17e7ac77d3a
ShellyBrady <mchellebr@hotmail.com>
2022-10-25 08:34:46 +0000

    add time.sleep to add pauses between printed lines

--- run.py
+++ run.py
@@ -6,17 +6,18 @@ choice2 = ['left', 'right']
 choice3 = ['run', 'stand', 'jog', 'talk']
 
 # Opening intro and input name
-print("Welcome to Your Next Adventure!\n"
-    "Every decision you make along the way will determine how your story goes.\n")
+print("Welcome to Your Next Adventure!\n")
+time.sleep(1)
+print("Every decision you make will determine how your story goes.\n")
 time.sleep(2)
 name1 = input("What shall I call you?\n")
 
 # Greeting and first choice
-print("\nWell hello! Nice to meet you, " + name1)
-time.sleep(1)
-print("\nGood news" + name1 + "! Its time for your monthly guilt-induced jog."
-    "\nPut on your barely worn runners and head for the door.\n"
-    "Are you ready to go?")
+print("\nWell hello, " + name1 + "! Nice to meet you.")
+time.sleep(2)
+print("\nGood news, " + name1 + "! Time for your monthly guilt-induced run!")
+print("\nPut on your barely worn runners and head for the door.\n")
+print("Are you ready to go?")
 time.sleep(1)
 
 # Action taken depending on input for choice1
@@ -48,7 +49,9 @@ while True:
 
     if choice2 == 'left':
         print("\nLess cars but more hills..your legs will hurt tomorrow!\n")
+        time.sleep(1)
         print("\nWhen your muscles burn, you stop to catch your breath.")
+        time.sleep(1)
         print("\nJust then, you hear a rustling from the trees nearby.")
         print("\nYour heart pounds as you see a bear lumbering towards you!")
         time.sleep(1)
@@ -56,12 +59,13 @@ while True:
 
     if choice2 == 'right':
         print("\nRight you are! Keep a steady pace.\n")
+        time.sleep(1)
         print("\nAt the end of the sleepy street you reach the main road.\n")
         print("Cars whizz past as you jog on to the hard shoulder.\n")
+        time.sleep(1)
         print("\nSuddenly, a white van screeches to a halt in front of you\n")
         time.sleep(1)
         choice3 = input("Type 'jog' to go on or 'talk' to hear what he wants:")
     else:
         choice2 = input("Choose to go 'left' or 'right':")
-
 
