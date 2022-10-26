3180356cde4b9fb290da1d6d6b9b4a0c7e08c09a
ShellyBrady <mchellebr@hotmail.com>
2022-10-26 23:04:14 +0000

    add pauses between strings and center some strings

--- run.py
+++ run.py
@@ -6,38 +6,47 @@ import sys
 # 'Play again' question to be posed when game is ended
 def play_again():
     repeat = ['play', 'quit']
-    repeat = input("Type 'play' to play again or 'quit' to give up:")
+    repeat = input("\nType 'play' to play again or 'quit' to give up: ")
     if repeat == 'play':
         choice1_result()
         my_game()
     if repeat == 'quit':
-        print('Thank you for playing. Goodbye!')
+        print('***********Thank you for playing. Goodbye!***********')
         sys.exit()
+    else:
+        repeat = input("Type 'play' to play again or 'quit' to give up: ")
 
 
 # Game-over function
 def game_over():
-    print("If only you had made different choices today!")
-    print("*********GAME OVER********")
+    print("\nIf only you had made different choices today!")
+    time.sleep(1)
+    print("\n*******************GAME OVER*******************\n")
+    time.sleep(1)
     play_again()
 
 
 # Winning is getting home safe in one piece!
 def you_win():
-    print("\nYou run for home faster than you have ever run before")
+    time.sleep(1)
+    print("\nYou run for home faster than you have ever run before!")
+    time.sleep(1)
     print("You fling open the door and kick your runners off with relief.")
+    time.sleep(1)
     print("Congratulate yourself for making it through another monthly jog.\n")
-    print("\n********YOU SURVIVED********\n")
+    time.sleep(1)
+    print("\n******************YOU SURVIVED******************\n")
+    time.sleep(1)
     play_again()
 
 
 # Opening intro and input name
 def intro():
-    print("****Welcome to Your Next Adventure!****\n")
+    print("*********Welcome to Your Next Adventure!*********\n")
     time.sleep(1)
     print("Every decision you make will determine how your story goes.\n")
     time.sleep(2)
-    name1 = input("What shall I call you?\n")
+    name1 = input("What shall I call you? ")
 
 # Greeting and first question
     print("Hello, " + name1.capitalize() + "! Nice to meet you.")
@@ -50,22 +59,26 @@ def intro():
 def choice1_result():
     choice1 = ['no', 'yes']
     print("\nPut on your barely worn runners and head for the door.\n")
-    print("Are you ready to go?")
+    print("****************Are you ready to go?****************")
 
     # Action taken depending on input for choice1
     while True:
         if choice1 == 'yes':
             print("\nBetter get going before it starts to get dark!\n")
+            time.sleep(1)
             print("\nHead for the quiet county road at the end of your drive.")
-            print("Left brings you to hilly narrow road surrounded by fields")
-            print("Right brings you to a quiet village to the busy main road")
+            time.sleep(2)
+            print("\nLeft brings you to a hilly road surrounded by fields.")
+            time.sleep(1)
+            print("Right brings you from quiet village to the busy main road.")
+            time.sleep(2)
             break
         if choice1 == 'no':
-            print("\nMaybe tomorrow. It looks like it might rain.\n")
+            print("\n*****Maybe tomorrow. It looks like it might rain.*****\n")
             time.sleep(1)
             game_over()
         else:
-            choice1 = input("Type 'no' or type 'yes':")
+            choice1 = input("Type 'no' or type 'yes': ")
 
 
 def my_game():
@@ -84,19 +97,30 @@ def my_game():
             print("\nWhen your muscles burn, you stop to catch your breath.")
             time.sleep(1)
             print("\nJust then, you hear a rustling from the trees nearby.")
+            time.sleep(1)
+            print("....")
+            time.sleep(1)
             print("\nYour heart pounds-you see a bear lumbering towards you!")
             time.sleep(1)
             choice3 = input("Type 'run' to run away, or 'stand' to stand: ")
             if choice3 == 'run':
-                print("You turn and run for home but the hills slow you down")
+                print("You turn and run for home but the hills slow you down.")
+                time.sleep(2)
                 print("The bear gains on you with surprising speed!")
+                time.sleep(1)
                 print("You become the bear's dinner")
+                time.sleep(1)
                 game_over()
             if choice3 == 'stand':
                 print("Every inch of your being screams at you to run.")
+                time.sleep(1)
                 print("You remember from somewhere that you need to be still.")
+                time.sleep(1)
+                print('....')
                 print("You can feel the bear's breath on your neck!")
+                time.sleep(2)
                 print("You think it's ended for you but the bear ambles off!")
+                time.sleep(1)
                 you_win()
             else:
                 choice3 = input("Type 'run' to run away, or 'stand' to stand:")
@@ -108,17 +132,24 @@ def my_game():
             time.sleep(1)
             print("Suddenly, a white van screeches to a halt in front of you!")
             time.sleep(1)
-            choice3 = input("Type 'jog' to go on or 'talk' to talk to him:")
+            choice3 = input("Type 'jog' to go on or 'talk' to talk to him: ")
             if choice3 == 'jog':
                 print("You watch too much true crime to get close to the van!")
+                time.sleep(1)
                 print("The driver door swings open as you jog past.")
+                time.sleep(1)
                 print("A man with a crazed look in his eyes jumps out!")
+                time.sleep(1)
                 you_win()
             if choice3 == 'talk':
                 print("You approach the driver's door just as it flings open!")
+                time.sleep(1)
                 print("You jump back but the driver is too fast for you.")
+                time.sleep(1)
                 print("He pulls you into the van with sudden speed!")
+                time.sleep(2)
                 print("The homicidal maniac takes you to his murder-house.")
+                time.sleep(1)
                 game_over()
             else:
                 choice3 = input("Type 'jog' to go on or 'talk' to talk: ")
